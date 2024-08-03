ddf7a665623ea5d96ec0eef24fc1a8966d114a37
sivakavindra@gmail.com <sivakavindra@gmail.com>
2024-08-03 14:48:06 +0530

    first commit

---
+++ HIDDEN COST/darkpattern.py
@@ -0,0 +1,201 @@
+from PIL import Image
+ref_fee = 24
+profit_fee = 8
+ship_fee = 2
+storage_fee = 192 
+tax = 3200
+a = input()
+if (a=="https://amzn.eu/d/cd1wbTS"):
+    image_path = "D:\project\Dark Pattern\iphone 13 red 128.jpg"
+    img=Image.open(image_path)
+    img.show()
+    price = 49000
+    print("Price When Realeased : ₹79,900")
+    print("Price as per Amazon : ₹" + str(price))
+    print("Referral Fees in Amazon : ₹" + str(ref_fee))
+    dealer_prof = price * (profit_fee/100)
+    print("Dealer Profit per Product : ₹" + str(dealer_prof))
+    shipping_fee = price * (ship_fee/100)
+    print("Shipping Fees in Product : ₹" + str(shipping_fee))
+    print("Storage Fees : ₹" + str(storage_fee))
+    resale_val = price - (dealer_prof + shipping_fee + storage_fee)
+    print("Resale Value : ₹" + str(resale_val))
+    
+if (a=="https://amzn.eu/d/2GkwBVa"):
+    image_path = "D:\project\Dark Pattern\iphone 13 green 128.jpg"
+    img=Image.open(image_path)
+    img.show()
+    price = 51500
+    print("Price When Realeased : ₹69,900")
+    print("Price as per Amazon : ₹" + str(price))
+    print("Referral Fees in Amazon : ₹" + str(ref_fee))
+    dealer_prof = price * (profit_fee/100)
+    print("Dealer Profit per Product : ₹" + str(dealer_prof))
+    shipping_fee = price * (ship_fee/100)
+    print("Shipping Fees in Product : ₹" + str(shipping_fee))
+    print("Storage Fees : ₹" + str(storage_fee))
+    resale_val = price - (dealer_prof + shipping_fee + storage_fee)
+    print("Resale Value : ₹" + str(resale_val))
+    
+if (a=="https://amzn.eu/d/fptJruZ"):
+    image_path = "D:\project\Dark Pattern\iphone 13 blue 256.jpg"
+    img=Image.open(image_path)
+    img.show()
+    price = 61999
+    print("Price When Realeased : ₹79,900")
+    print("Price as per Amazon : ₹" + str(price))
+    print("Referral Fees in Amazon : ₹" + str(ref_fee))
+    dealer_prof = price * (profit_fee/100)
+    print("Dealer Profit per Product : ₹" + str(dealer_prof))
+    shipping_fee = price * (ship_fee/100)
+    print("Shipping Fees in Product : ₹" + str(shipping_fee))
+    print("Storage Fees : ₹" + str(storage_fee))
+    resale_val = price - (dealer_prof + shipping_fee + storage_fee)
+    print("Resale Value : ₹" + str(resale_val))
+
+
+if (a=="https://amzn.eu/d/fbIsxH6"):
+    image_path = "D:\project\Dark Pattern\iphone 13 red 256.jpg"
+    img=Image.open(image_path)
+    img.show()
+    price = 61999
+    print("Price When Realeased : ₹79,900")
+    print("Price as per Amazon : ₹" + str(price))
+    print("Referral Fees in Amazon : ₹" + str(ref_fee))
+    dealer_prof = price * (profit_fee/100)
+    print("Dealer Profit per Product : ₹" + str(dealer_prof))
+    shipping_fee = price * (ship_fee/100)
+    print("Shipping Fees in Product : ₹" + str(shipping_fee))
+    print("Storage Fees : ₹" + str(storage_fee))
+    resale_val = price - (dealer_prof + shipping_fee + storage_fee)
+    print("Resale Value : ₹" + str(resale_val))
+
+if (a=="https://amzn.eu/d/0YeMkBb"):
+    image_path = "D:\project\Dark Pattern\iphone 13 green 256.jpg"
+    img=Image.open(image_path)
+    img.show()
+    price = 62499
+    print("Price When Realeased : ₹69,900")
+    print("Price as per Amazon : ₹" + str(price))
+    print("Referral Fees in Amazon : ₹" + str(ref_fee))
+    dealer_prof = price * (profit_fee/100)
+    print("Dealer Profit per Product : ₹" + str(dealer_prof))
+    shipping_fee = price * (ship_fee/100)
+    print("Shipping Fees in Product : ₹" + str(shipping_fee))
+    print("Storage Fees : ₹" + str(storage_fee))
+    resale_val = price - (dealer_prof + shipping_fee + storage_fee)
+    print("Resale Value : ₹" + str(resale_val))
+
+if (a=="https://amzn.eu/d/aRt6IOz"):
+    image_path = "D:\project\Dark Pattern\iphone 13 midnight 256.jpg"
+    img=Image.open(image_path)
+    img.show()
+    price = 61999
+    print("Price When Realeased : ₹69,900")
+    print("Price as per Amazon : ₹" + str(price))
+    print("Referral Fees in Amazon : ₹" + str(ref_fee))
+    dealer_prof = price * (profit_fee/100)
+    print("Dealer Profit per Product : ₹" + str(dealer_prof))
+    shipping_fee = price * (ship_fee/100)
+    print("Shipping Fees in Product : ₹" + str(shipping_fee))
+    print("Storage Fees : ₹" + str(storage_fee))
+    resale_val = price - (dealer_prof + shipping_fee + storage_fee)
+    print("Resale Value : ₹" + str(resale_val))
+
+
+
+if (a=="https://amzn.eu/d/iAeZMkT"):
+    image_path = "D:\project\Dark Pattern\iphone 13 pink 256.jpg"
+    img=Image.open(image_path)
+    img.show()
+    price = 61999
+    print("Price When Realeased : ₹79,900")
+    print("Price as per Amazon : ₹" + str(price))
+    print("Referral Fees in Amazon : ₹" + str(ref_fee))
+    dealer_prof = price * (profit_fee/100)
+    print("Dealer Profit per Product : ₹" + str(dealer_prof))
+    shipping_fee = price * (ship_fee/100)
+    print("Shipping Fees in Product : ₹" + str(shipping_fee))
+    print("Storage Fees : ₹" + str(storage_fee))
+    resale_val = price - (dealer_prof + shipping_fee + storage_fee)
+    print("Resale Value : ₹" + str(resale_val))
+
+if (a=="https://amzn.eu/d/eaAka4O"):
+    image_path = "D:\project\Dark Pattern\iphone 13 starlight 256.jpg"
+    img=Image.open(image_path)
+    img.show()
+    price = 61999
+    print("Price When Realeased : ₹69,900")
+    print("Price as per Amazon : ₹" + str(price))
+    print("Referral Fees in Amazon : ₹" + str(ref_fee))
+    dealer_prof = price * (profit_fee/100)
+    print("Dealer Profit per Product : ₹" + str(dealer_prof))
+    shipping_fee = price * (ship_fee/100)
+    print("Shipping Fees in Product : ₹" + str(shipping_fee))
+    print("Storage Fees : ₹" + str(storage_fee))
+    resale_val = price - (dealer_prof + shipping_fee + storage_fee)
+    print("Resale Value : ₹" + str(resale_val))
+
+if (a=="https://amzn.eu/d/69nNrfw"):
+    image_path = "D:\project\Dark Pattern\iphone 13 red 512.jpg"
+    img=Image.open(image_path)
+    img.show()
+    price = 69999
+    print("Price When Realeased : ₹89,900")
+    print("Price as per Amazon : ₹" + str(price))
+    print("Referral Fees in Amazon : ₹" + str(ref_fee))
+    dealer_prof = price * (profit_fee/100)
+    print("Dealer Profit per Product : ₹" + str(dealer_prof))
+    shipping_fee = price * (ship_fee/100)
+    print("Shipping Fees in Product : ₹" + str(shipping_fee))
+    print("Storage Fees : ₹" + str(storage_fee))
+    resale_val = price - (dealer_prof + shipping_fee + storage_fee)
+    print("Resale Value : ₹" + str(resale_val))
+
+if (a=="https://amzn.eu/d/4CzKNct"):
+    image_path = "D:\project\Dark Pattern\iphone 14 red 256.jpg"
+    img=Image.open(image_path)
+    img.show()
+    price = 65999
+    print("Price When Realeased : ₹89,900")
+    print("Price as per Amazon : ₹" + str(price))
+    print("Referral Fees in Amazon : ₹" + str(ref_fee))
+    dealer_prof = price * (profit_fee/100)
+    print("Dealer Profit per Product : ₹" + str(dealer_prof))
+    shipping_fee = price * (ship_fee/100)
+    print("Shipping Fees in Product : ₹" + str(shipping_fee))
+    print("Storage Fees : ₹" + str(storage_fee))
+    resale_val = price - (dealer_prof + shipping_fee + storage_fee)
+    print("Resale Value : ₹" + str(resale_val))
+
+if (a=="https://amzn.eu/d/cRIhgh7"):
+    image_path = "D:\project\Dark Pattern\iphone 14 midnight 128.jpg"
+    img=Image.open(image_path)
+    img.show()
+    price = 57999
+    print("Price When Realeased : ₹69,900")
+    print("Price as per Amazon : ₹" + str(price))
+    print("Referral Fees in Amazon : ₹" + str(ref_fee))
+    dealer_prof = price * (profit_fee/100)
+    print("Dealer Profit per Product : ₹" + str(dealer_prof))
+    shipping_fee = price * (ship_fee/100)
+    print("Shipping Fees in Product : ₹" + str(shipping_fee))
+    print("Storage Fees : ₹" + str(storage_fee))
+    resale_val = price - (dealer_prof + shipping_fee + storage_fee)
+    print("Resale Value : ₹" + str(resale_val))
+
+if (a=="https://amzn.eu/d/19Jzovj"):
+    image_path = "D:\project\Dark Pattern\iphone 14 starlight 512.jpg"
+    img=Image.open(image_path)
+    img.show()
+    price = 86999
+    print("Price When Realeased : ₹99,900")
+    print("Price as per Amazon : ₹" + str(price))
+    print("Referral Fees in Amazon : ₹" + str(ref_fee))
+    dealer_prof = price * (profit_fee/100)
+    print("Dealer Profit per Product : ₹" + str(dealer_prof))
+    shipping_fee = price * (ship_fee/100)
+    print("Shipping Fees in Product : ₹" + str(shipping_fee))
+    print("Storage Fees : ₹" + str(storage_fee))
+    resale_val = price - (dealer_prof + shipping_fee + storage_fee)
+    print("Resale Value : ₹" + str(resale_val))
